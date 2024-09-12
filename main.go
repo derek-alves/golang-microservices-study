e9911098404761e308581cb6497d2583b7214394
Derek <dev.derek.alves@gmail.com>
2024-09-12 00:25:00 -0300

    add app start in main

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/derek-alves/golang-microservices-study/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Get("/hello", basicHandler)
-	router.Use(middleware.Logger)
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
+
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app:", err)
 	}
-
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world"))
 }
