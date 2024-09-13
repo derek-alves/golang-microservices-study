af2f31a9470572022d48dc96e4675a6735d489d7
Derek <dev.derek.alves@gmail.com>
2024-09-13 00:49:41 -0300

    add regis client

--- application/app.go
+++ application/app.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
@@ -22,9 +26,29 @@ func (app *App) Start(ctx context.Context) error {
 		Addr:    ":3000",
 		Handler: app.router,
 	}
-	err := server.ListenAndServe()
+	err := app.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
+
+	fmt.Println("Starting server")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+	}
+
 	return nil
+
 }
