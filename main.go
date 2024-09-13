f4d91d6d373c0436d13cbb30e4526de06b593614
Derek <dev.derek.alves@gmail.com>
2024-09-13 00:49:32 -0300

    add signal with context

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"os/signal"
 
 	"github.com/derek-alves/golang-microservices-study/application"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
